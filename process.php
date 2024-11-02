a83347603065ce70b368b55b2b591253fd795b38
IPS-byte <samuelodesina686@gmail.com>
2024-11-02 12:56:01 -0400

    update

--- process.php
+++ process.php
@@ -13,5 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // display the students information
     echo"<h2>Student information</h2>";
     echo"<p><strong>Name:</strong>$name</p>";
+    echo"<p><strong>age:</strong>$age</p>";
+    echo"<p><strong>studentId:</strong>$studentId</p>";
+    echo"<p><strong>grades:</strong>" . implode(', ', $grades) ."</p>";
+    echo"<p><strong>average Grade:</strong>" . number_format($average_grade, 2) ."</p>";
+    //  validation
+    if ($age < 0) {
+        # code...
+        echo "<p style='color:red;'>age cant be negative</p>";
+    }
+    if (!is_numeric($studentId)) {
+        # code...
+        echo "<p style='color:red;'>studentId should be a number</p>";
+
+    }
 }
 ?>
