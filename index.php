13385e47f5d69e528035da29adbea3290470fb86
IPS-byte <samuelodesina686@gmail.com>
2024-11-02 11:44:54 -0400

    Update

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Information</title>
+    <link rel="stylesheet" href="../css/stlyesheet.css">
+</head>
+<body>
+    <div class="form-container">
+        <h2>Student Information</h2>
+        <form action="process.php" method="POST">
+          <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+          </div>
+          <div class="form-group">
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" required>
+          </div>
+          <div class="form-group">
+            <label for="studentId">Student ID:</label>
+            <input type="text" id="studentId" name="studentId" required>
+          </div>
+          <div class="form-group">
+            <label for="grades">Grades:</label>
+            <input type="text" id="grades" name="grades" required>
+          </div>
+          <div class="form-group">
+            <input type="submit" value="Submit">
+          </div>
+        </form>
+      </div>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    # code...
+    // retrive form data
+    $name= $_POST["name"];
+    $age=(int)$_POST["age"];
+    $studentId=$_POST["studentId"];
+    $grades=$_POST["grades"];
+    // convert grades to an array of float
+    $grades=array_map("floatval", explode(",",$grades_input));
+    // calucate the average grade
+    $average_grade=array_sum($grades)/count($grades);
+    // display the students information
+    echo"<h2>Student information</h2>";
+    echo"<p><strong>Name:</strong>$name</p>";
+}
+?>
